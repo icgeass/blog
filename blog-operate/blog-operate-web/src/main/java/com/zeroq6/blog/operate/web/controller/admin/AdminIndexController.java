03cd4c6904525cff6581bee3e5190db3c055a098
icgeass <icgeass@hotmail.com>
2017-09-05 16:35:32 +0800

    登出, jar包更新

--- blog-operate/blog-operate-web/src/main/java/com/zeroq6/blog/operate/web/controller/admin/AdminIndexController.java
+++ blog-operate/blog-operate-web/src/main/java/com/zeroq6/blog/operate/web/controller/admin/AdminIndexController.java
@@ -1,8 +1,12 @@
 package com.zeroq6.blog.operate.web.controller.admin;
 
+import com.zeroq6.sso.web.client.interceptor.LoginInterceptor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by yuuki ausna on 2017/8/11.
@@ -17,4 +21,12 @@ public class AdminIndexController {
     public String index() throws Exception {
         return "redirect:/admin/post" ;
     }
+
+    @RequestMapping(value = "logout", method = {RequestMethod.GET})
+    public String logout(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        LoginInterceptor.logout(request, response);
+        return null ;
+    }
+
+
 }
