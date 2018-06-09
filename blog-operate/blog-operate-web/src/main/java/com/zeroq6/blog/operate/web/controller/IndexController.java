4b10f4f8c705355e3d14121269f226c92f9aa88e
icgeass <icgeass@hotmail.com>
2018-06-09 11:38:36 +0800

    404页面相关信息

--- blog-operate/blog-operate-web/src/main/java/com/zeroq6/blog/operate/web/controller/IndexController.java
+++ blog-operate/blog-operate-web/src/main/java/com/zeroq6/blog/operate/web/controller/IndexController.java
@@ -1,11 +1,14 @@
 package com.zeroq6.blog.operate.web.controller;
 
 import com.zeroq6.blog.common.base.BaseController;
+import com.zeroq6.blog.operate.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.annotation.Resource;
 
 /**
  * @author icgeass@hotmail.com
@@ -13,6 +16,16 @@ import javax.annotation.Resource;
  */
 @Controller("/")
 public class IndexController extends BaseController{
+
+    @Autowired
+    private PostService postService;
+
+    @ModelAttribute
+    public void loadState(Model model) {
+        model.addAttribute(NAME_MENU, "404");
+        model.addAttribute(NAME_CATEGORY_TITLE, "404");
+        model.addAllAttributes(postService.getSidebarInfo().getBody());
+    }
 
     @RequestMapping(value = "", method = {RequestMethod.GET})
     public String index() throws Exception {
