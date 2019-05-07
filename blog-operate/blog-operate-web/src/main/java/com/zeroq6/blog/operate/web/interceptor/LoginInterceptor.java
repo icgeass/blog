d2d205ae9e7b09a881617f3e7a998d6456e281a8
icgeass <icgeass@hotmail.com>
2019-05-07 17:16:34 +0800

    打开druid的SQL监控

--- blog-operate/blog-operate-web/src/main/java/com/zeroq6/blog/operate/web/interceptor/LoginInterceptor.java
+++ blog-operate/blog-operate-web/src/main/java/com/zeroq6/blog/operate/web/interceptor/LoginInterceptor.java
@@ -5,8 +5,10 @@ import com.zeroq6.common.web.CookieUtils;
 import com.zeroq6.common.web.IpUtils;
 import com.zeroq6.common.web.RequestUtils;
 import com.zeroq6.common.web.ResponseUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -16,11 +18,14 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.List;
 
 
 @Service
-public class LoginInterceptor implements HandlerInterceptor {
+public class LoginInterceptor implements HandlerInterceptor, InitializingBean {
 
 
     @Autowired
@@ -31,9 +36,6 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     @Value("${login.cookie.name}")
     private String loginCookieName = "";
-
-    @Value("${login.uri.prefix}")
-    private String loginUriPrefix;
 
 
     @Value("${login.cookie.path}")
@@ -47,11 +49,28 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Value("${login.return.url.name}")
     private String loginReturnUrlName;
 
+    private String disallowUriPrefix;
+
+
+    private final List<String> disallowUriPrefixList = new ArrayList<String>();
+
+
+    public void setDisallowUriPrefix(String disallowUriPrefix) {
+        this.disallowUriPrefix = disallowUriPrefix;
+    }
+
+
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
         try {
             String uri = httpServletRequest.getRequestURI();
-            if (uri.startsWith(loginUriPrefix)) {
+            boolean intercept = false;
+            for (String disallow : disallowUriPrefixList) {
+                if (uri.startsWith(disallow)) {
+                    intercept = true;
+                }
+            }
+            if (intercept) {
                 Cookie cookie = CookieUtils.getCookie(httpServletRequest, loginCookieName);
                 if (null == cookie) {
                     ResponseUtils.doRedirect(httpServletRequest, httpServletResponse, getReturnUrl(httpServletRequest));
@@ -63,8 +82,9 @@ public class LoginInterceptor implements HandlerInterceptor {
                     ResponseUtils.doRedirect(httpServletRequest, httpServletResponse, getReturnUrl(httpServletRequest));
                     return false;
                 }
-
             }
+
+
             return true;
         } catch (Exception e) {
             logger.error("拦截器preHandle异常", e);
@@ -103,4 +123,12 @@ public class LoginInterceptor implements HandlerInterceptor {
     }
 
 
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // disallow的uri
+        if (StringUtils.isNotBlank(disallowUriPrefix)) {
+            disallowUriPrefixList.addAll(Arrays.asList(disallowUriPrefix.split("\\s*,\\s*")));
+        }
+        logger.info("拦截器初始化完成");
+    }
 }
