bde228602cbc4db1260d534bea4370f80d3153fb
icgeass <icgeass@hotmail.com>
2019-06-16 19:21:02 +0800

    自动部署脚本

--- doc/sh/deploy-blog.sh
+++ doc/sh/deploy-blog.sh
@@ -7,4 +7,7 @@ mvn clean -U install -Dmaven.test.skip=true -Pproduction
 cp -r /export/git/blog/blog-operate/blog-operate-web/target/blog-operate-web-1.0.0-SNAPSHOT/* /export/App/www.6zeroq.com/
 cp /export/lock/www.6zeroq.com/web.xml /export/App/www.6zeroq.com/WEB-INF/web.xml
 cp /export/lock/www.6zeroq.com/spring-mvc.xml /export/App/www.6zeroq.com/WEB-INF/classes/spring/spring-mvc.xml
+cp /export/lock/www.6zeroq.com/rsa_private_key.pem /export/App/www.6zeroq.com/WEB-INF/classes/cer/rsa_private_key.pem
+cp /export/lock/www.6zeroq.com/pkcs8_rsa_private_key.pem /export/App/www.6zeroq.com/WEB-INF/classes/cer/pkcs8_rsa_private_key.pem
+cp /export/lock/www.6zeroq.com/rsa_public_key.pem /export/App/www.6zeroq.com/WEB-INF/classes/cer/rsa_public_key.pem
 /export/Domains/www.6zeroq.com/server1/bin/restart.sh
