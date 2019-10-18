7c6037c38b1242d02e7b3c0f6bb4fd3a0d183bd2
icgeass <icgeass@hotmail.com>
2019-10-18 10:23:23 +0800

    Properties存入非String时，containsKey返回true，但是getProperties返回null

--- blog-operate/blog-operate-web/src/test/java/NoSpringTest.java
+++ blog-operate/blog-operate-web/src/test/java/NoSpringTest.java
@@ -1,10 +1,13 @@
 
 import com.zeroq6.blog.operate.service.login.LoginService;
 import com.zeroq6.common.counter.CounterService;
+import com.zeroq6.common.counter.mybatis.PropertyParser;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
 
 public class NoSpringTest {
 
@@ -31,6 +34,24 @@ public class NoSpringTest {
         System.out.println(CounterService.DateFormat.toDescBySeconds(0));
         System.out.println(CounterService.DateFormat.toDescBySeconds(60 * 60 + 60 * 59 + 1));
 
+
+
+
+    }
+
+
+    @Test
+    public void testProp() throws Exception {
+        Properties properties = new Properties();
+        properties.put("a", "aaa");
+        properties.put("b", 1);
+        System.out.println(properties.containsKey("a"));
+        System.out.println(properties.getProperty("a"));
+        System.out.println(properties.containsKey("b"));
+        System.out.println(properties.getProperty("b"));
+
+
+        System.out.println(PropertyParser.parse("a=@{a},b=@{b}", properties));
 
     }
 
