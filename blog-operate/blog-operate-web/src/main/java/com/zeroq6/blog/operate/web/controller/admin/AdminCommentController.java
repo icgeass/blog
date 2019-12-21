e9fa551095081ac82bbaa2a3c2e22fe46c9258c0
icgeass <icgeass@hotmail.com>
2019-12-21 12:55:32 +0800

    留言和文章区分

--- blog-operate/blog-operate-web/src/main/java/com/zeroq6/blog/operate/web/controller/admin/AdminCommentController.java
+++ blog-operate/blog-operate-web/src/main/java/com/zeroq6/blog/operate/web/controller/admin/AdminCommentController.java
@@ -3,11 +3,14 @@ package com.zeroq6.blog.operate.web.controller.admin;
 import com.zeroq6.blog.common.domain.CommentDomain;
 import com.zeroq6.blog.operate.service.CommentService;
 import com.zeroq6.blog.common.base.Page;
+import com.zeroq6.blog.operate.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by yuuki asuna on 2017/8/10.
@@ -20,9 +23,18 @@ public class AdminCommentController {
     @Autowired
     private CommentService commentService;
 
+    @Autowired
+    private PostService postService;
+
     @RequestMapping("")
     public String list(CommentDomain commentDomain, Page<CommentDomain> page, Model view) {
         commentService.selectPage(commentDomain, page);
+        List<CommentDomain> commentDomainList = page.getData();
+        if (null != commentDomainList && !commentDomainList.isEmpty()) {
+            for (CommentDomain item : commentDomainList) {
+                item.put("postDomain", postService.selectByKey(item.getPostId()));
+            }
+        }
         view.addAttribute("page", page);
         view.addAttribute("commentDomain", commentDomain);
         return "/admin/comment/commentList";
